5cf0c3a083c7f6bed0fb09c17f5625bdccd84c66
PARSIVALJ <uchejesusboy@gmail.com>
2023-10-11 10:29:47 +0300

    Created Animal class and a TestAnimal class to test the Animal class

---
+++ Lab2/src/Animal.java
@@ -0,0 +1,23 @@
+
+public class Animal {
+	public String name;
+	public int age;
+	public double weight;
+	
+	public Animal() {
+		System.out.println("A new animal has been created. ");
+	}
+	
+	public void isSleeping() {
+		System.out.println(name + " is sleeping.");
+	}
+	
+	public void isPlaying() {
+		System.out.println(name + " is playing.");
+	}
+	
+	public void isRunning() {
+		System.out.println(name + " is Running.");
+	}
+
+}
---
+++ Lab2/src/TestAnimal.java
@@ -0,0 +1,19 @@
+
+public class TestAnimal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Animal newAnimal;
+		newAnimal = new Animal();
+		
+		newAnimal.name = "Zigi";
+		newAnimal.age = 13;
+		newAnimal.weight = 12.5;
+		
+		newAnimal.isPlaying();
+		newAnimal.isSleeping();
+		newAnimal.isRunning();
+
+	}
+
+}
