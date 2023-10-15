b56c64a5aacf68090ebe4c96a01468e153020adc
PARSIVALJ <uchejesusboy@gmail.com>
2023-10-15 20:30:17 +0300

    Reuse the port class, added new parameters, constructors, and new methods/behaviors. Create a new class called Ship where ship's information will be stored. Add a constructor and methods to set and get ship id

--- Lab2/src/Port.java
+++ Lab2/src/Port.java
@@ -1,24 +1,59 @@
-
 public class Port {
-	public String name;
-	public int noOfParkingSlots;
+	// Parameters
 	
-	Buildings bld = new Buildings();
+	//public 
+	public String name;  // The name of the port.
+	public String location; // The geographic location of the port.
+	public int capacity;  // The maximum capacity of the port in terms of cargo or ships
+	public String status; // The current operational status of the port (e.g., "Open," "Closed," "Under maintenance").
+	public int dockCount; // The number of docks available at the port.
+	public Ship[] dockedShips = new Ship[dockCount]; // A array of docked ships
 	
-	public Port() {
-		System.out.println("This is a new port.");
+	//private
+	private String[] statusTypes = {"Open", "Closed", "Under Maintenance"}; // An array of all the operational status' that the port can be in.
+	private int numberOfDockedShips = 0;
+	// Constructors
+	
+	public Port() { // Constructor without parameters
+		name = "";
+		location = "";
+		capacity = 0;
+		status = "Closed";
+		dockCount = 0;
+		for (int i = 0; i < dockedShips.length; i++) {
+		    dockedShips[i] = null;
+		}
 	}
 	
-	public void getNameOfBuildings() {
-		System.out.println("Building name is " + bld.name +".");
+	public Port(String name, String location, int capacity, String status, int dockCount) { // Constructor with parameters
+		this.name = name;
+		this.location = location;
+		this.capacity = capacity;
+		this.status = status;
+		this.dockCount = dockCount;
 	}
 	
-	public void timeForBuildingClosure() {
-		System.out.println("Building closes at "+bld.timeOfClosure+".");
+	// Methods/Behaviors
+	
+	public void openPort() { // Method to open port
+		status = statusTypes[0];
+		System.out.println("Port Status: Open");
 	}
 	
-	public void printPortName() {
-		System.out.println("The name of this port is "+ name+".");
+	public void closePort() {
+		status = statusTypes[1];
+		System.out.println("Port Status: Closed");
 	}
-
+	
+	public void dockShip(Ship ship) {
+		for (int i = numberOfDockedShips; i < dockedShips.length;) {
+			ship.setShipId(i);
+			dockedShips[i] = ship;
+			numberOfDockedShips ++;
+			break;
+		}
+		System.out.println("Ship docked successfully.");
+	}
+	
+	
 }
---
+++ Lab2/src/Ship.java
@@ -0,0 +1,23 @@
+
+public class Ship {
+	//Public 
+	
+	
+	// Private
+    private int shipId;
+    
+    // Constructors
+    public Ship() {
+    	shipId = -1;
+    }
+    
+    
+    //Methods/ Behaviors
+    public void setShipId(int id) {
+    	shipId = id;
+    }
+    
+    public int getshipId() {
+    	return shipId;
+    }
+}
