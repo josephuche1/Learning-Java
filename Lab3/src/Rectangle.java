8a4702a4a551051c6f700513451170fdba2830ef
PARSIVALJ <uchejesusboy@gmail.com>
2023-10-17 22:08:04 +0300

    Create Rectangle class. Add width, height and color paramaters. Add 1 no args constructor and 1 constructor that takes the height and width

---
+++ Lab3/src/Rectangle.java
@@ -0,0 +1,20 @@
+
+public class Rectangle {
+	// Parameters
+     public double width;
+     public double height;
+     public String color;
+     
+     // Constructors
+     public Rectangle() {
+    	 width = 1;
+    	 height = 1;
+    	 color = "white";
+     }
+     
+     public Rectangle(double width, double height) {
+    	 this.width = width;
+    	 this.height = height;
+    	 this.color = "white";
+     }
+}
