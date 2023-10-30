aba42c0a144a828fac72f79f6669dddd845f08c9
PARSIVALJ <uchejesusboy@gmail.com>
2023-10-30 12:31:12 +0200

    Create  MyRadio class to test the Radio class that was already created

--- Lab5/src/MyRadio.java
+++ Lab5/src/MyRadio.java
@@ -1,6 +1,21 @@
 
 public class MyRadio {
 	public static void main(String[] args) {
+		Radio newRadio = new Radio();
+		System.out.println("Classic FM: "+Radio.CLASSIC_FM);
+		System.out.println("BBC Radio 3: "+Radio.frequencyOfBBC(3));
+		
+		newRadio.setFrequency(Radio.BRMB);
+		newRadio.listen();
+		
+		newRadio.setFrequency(Radio.frequencyOfBBC(2));
+		newRadio.listen();
+		
+		newRadio.setFrequency(92.7f);
+		newRadio.listen();
+		
+		newRadio.setFrequency(94.5f);
+		newRadio.listen();
 		
 	}
 }
