0688d442aa8fec1e1b2d6eeccebedd124ce36419
PARSIVALJ <uchejesusboy@gmail.com>
2023-11-06 11:31:38 +0200

    Create horse class

---
+++ Lab6/src/Horse.java
@@ -0,0 +1,28 @@
+
+public class Horse {
+	private String name;
+	private  String color;
+	private int birthYear;
+	
+	// Setters
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public void setBirthYear(int birthYear) {
+		this.birthYear = birthYear;
+	}
+	
+	// Getters
+	public String getName() {
+		return this.name;
+	}
+	public String getColor() {
+		return this.color;
+	}
+	public int getBirthYear(){
+		return this.birthYear;
+	}
+}
