7bfedc9594df70a4ec53dccc987d3786d10102e9
PARSIVALJ <uchejesusboy@gmail.com>
2023-11-09 17:08:12 +0200

    Create Ship class

---
+++ Lab6/src/Ship.java
@@ -0,0 +1,29 @@
+
+public class Ship {
+    private String name;
+    private String yearBuilt;
+    
+    public Ship(String n, String y) {
+    	this.name = n;
+    	this.yearBuilt = y;
+    }
+    
+    public void setName(String n) {
+    	this.name = n;
+    }
+    
+    public void setYearBuilt(String y) {
+    	this.yearBuilt = y;
+    }
+    
+    public String getName() {
+    	return this.name;
+    }
+    public String getYearBuilt() {
+    	return this.yearBuilt;
+    }
+    
+    public String toString() {
+    	return "My name is "+this.name+" and I was built in "+this.yearBuilt+"."; 
+    }
+}
