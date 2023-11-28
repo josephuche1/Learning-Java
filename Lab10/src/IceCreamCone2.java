7eceb548820d83a96699734650aeb6b32c2d643a
PARSIVALJ <uchejesusboy@gmail.com>
2023-11-28 22:19:20 +0200

    Learning how to create custom exception handlers

---
+++ Lab10/src/IceCreamCone2.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+public class IceCreamCone2 {
+   String flavor;
+   int scoop;
+   final int MAX =3;
+   String[] flavor = {"Chocolate", "Strawberry", "Vanilla", "Butter pecan"};
+   
+}
--- Lab10/src/RadiusExceptionDemo.java
+++ Lab10/src/RadiusExceptionDemo.java
@@ -7,5 +7,5 @@ public class RadiusExceptionDemo {
 		System.out.println(newCircle.getRadius());
 
 	}
-
+   
 }
