8ecce09b287f7201df5d8e942df1ef3dc88dd34a
PARSIVALJ <uchejesusboy@gmail.com>
2023-11-28 22:59:57 +0200

    Created application to test classes with simple GUI using swing

--- Lab9/src/useInsurance.java
+++ Lab9/src/useInsurance.java
@@ -4,8 +4,26 @@ public class useInsurance {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int insurance;
-		String input = JOptionPane.showInputDialog(null, "Enter 1 for Life Insurance and 2 for Health Insurance")
+		String input = JOptionPane.showInputDialog(null, "Enter 1 for Life Insurance and 2 for Health Insurance");
+		insurance = Integer.parseInt(input);
+		
+		if(insurance == 1) {
+			insuranceLife();
+		}
+		else {
+			insuranceHealth();
+		}
         
+	}
+	
+	public static void insuranceLife() {
+		Life life = new Life();
+		life.display();
+	}
+	
+	public static void insuranceHealth() {
+		Health health = new Health();
+		health.display();
 	}
 
 }
