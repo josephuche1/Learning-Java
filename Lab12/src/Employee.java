c1f3d41b8f856df040b02c72cc1cfd995ce56281
PARSIVALJ <uchejesusboy@gmail.com>
2023-12-12 09:39:37 +0200

    implement Employee class

--- Lab12/src/Employee.java
+++ Lab12/src/Employee.java
@@ -3,8 +3,16 @@ public class Employee {
     private String idNum;
     private double hourlyWage;
     
-    public Employee(String idNum, double hourlyWage) {
-    	this.idNum = idNum;
-    	this.hourlyWage = hourlyWage;
+    public Employee(String idNum, double hourlyWage) throws EmployeeException{
+    	try {
+    		this.idNum = idNum;
+    		this.hourlyWage = hourlyWage;
+    		
+    		if(this.hourlyWage < 6 || this.hourlyWage > 50) {
+    			throw(new EmployeeException("Employee Exception: ID Number: \t"+this.idNum+ ", hourlyWage: "+ this.hourlyWage));
+    		}
+    	}catch(EmployeeException err) {
+    		System.out.println(err.getMessage());
+    	}
     }
 }
